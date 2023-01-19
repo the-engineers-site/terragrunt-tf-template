d6caf8e2d1a753ff94912bbc04cf775f9e0ce4dc
Yash Jagdale <jagdale0210@gmail.com>
2023-01-19 16:53:30 +0530

    lock files removed

--- terragrunt/non-prod/us-east-1/develop/vpc/outputs.tf
+++ terragrunt/non-prod/us-east-1/develop/vpc/outputs.tf
@@ -9,3 +9,15 @@ resource "aws_ssm_parameter" "vpc-id" {
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "available_azs" {
+  value = module.vpc.azs
+}
